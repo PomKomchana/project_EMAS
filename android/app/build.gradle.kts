8b6bb66428cf27104b6f4af1aa6520a6f8a10473
Komchana Yuwapornphongkul <pomkomy49@gmail.com>
2026-07-26 22:02:34 +0700

    add google map api

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile))
+}
+
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
@@ -21,6 +30,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        manifestPlaceholders["MAP_API_KEY"] = localProperties.getProperty("MAP_API_KEY") ?: ""
     }
 
     buildTypes {
